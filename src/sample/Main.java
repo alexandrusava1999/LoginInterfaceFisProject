4294d021f2834f960b544731cfe23876cab114ed
Stan Darius <standarius24@gmail.com>
2020-04-27 20:31:01 +0300

    FIS-17 Interface / Create Interface for Register

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,19 +5,20 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
 
 public class Main extends Application {
-
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FxmlAndControllerClass/Register.fxml"));
+        primaryStage.initStyle(StageStyle.DECORATED);
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();}
 
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         launch(args);
+
     }
+
 }
