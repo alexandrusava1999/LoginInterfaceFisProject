d8c6096ae4569e76d89e33345f5657c28b6f6b48
Stan Darius <standarius24@gmail.com>
2020-04-27 20:57:46 +0300

    FIS- 27 DataBase / Connect MySQL with Java using Xampp

---
+++ src/sample/FxmlAndControllerClass/ConnectionClass.java
@@ -0,0 +1,29 @@
+package sample.FxmlAndControllerClass;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConnectionClass {
+    public Connection connection;
+    public  Connection getConnection(){
+
+
+        String dbName="application";
+        String userName="root";
+        String password="";
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+
+            connection= DriverManager.getConnection("jdbc:mysql://localhost/"+dbName,userName,password);
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        return connection;
+    }
+
+}
