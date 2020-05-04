e6feab3e9d072890237656807d95fcb67440d17d
Alexandru <alexandru.sava.1999@gmail.com>
2020-05-04 17:56:40 +0300

    FIS-40: Accepting multiple connections - The Server can now accept multiple connections in the same time.
    Adding Maven build tool.

--- ChatServer/src/com/muc/ServerMain.java
+++ ChatServer/src/com/muc/ServerMain.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Date;
 
 public class ServerMain {
     public static void main(String[] args) {
@@ -14,12 +15,31 @@ public class ServerMain {
                 System.out.println("About to accept client connection...");
                 Socket clientSocket = serverSocket.accept();
                 System.out.println("Accepted connection from " + clientSocket);
-                OutputStream outputStream = clientSocket.getOutputStream();
-                outputStream.write("Hello World\n".getBytes());
-                clientSocket.close();
+                Thread t = new Thread() {
+                    @Override
+                    public void run() {
+                        try {
+                            handleClientSocket(clientSocket);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+                t.start();
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private static void handleClientSocket(Socket clientSocket) throws IOException, InterruptedException {
+        OutputStream outputStream = clientSocket.getOutputStream();
+        for(int i=0; i<10; i++) {
+            outputStream.write(("Time now is " + new Date() + "\n").getBytes());
+            Thread.sleep(1000);
+        }
+        clientSocket.close();
     }
 }
