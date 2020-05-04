6d5616238da9b54743685a12ea852363ef996dc7
Alexandru <alexandru.sava.1999@gmail.com>
2020-05-04 12:11:01 +0300

    Creating a simple chat server

---
+++ ChatServer/src/com/muc/ServerMain.java
@@ -0,0 +1,25 @@
+package com.muc;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerMain {
+    public static void main(String[] args) {
+        int port = 8818;
+        try {
+            ServerSocket serverSocket = new ServerSocket(port);
+            while(true) {
+                System.out.println("About to accept client connection...");
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("Accepted connection from " + clientSocket);
+                OutputStream outputStream = clientSocket.getOutputStream();
+                outputStream.write("Hello World\n".getBytes());
+                clientSocket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
